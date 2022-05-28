bce72932dc4e40ee42ca083c0d3fa66647102bb2
Odmaral Tumenjargal <odmaraltumenjargal@Odmarals-MacBook-Air.local>
2022-05-27 18:30:47 -0700

    auth and database

---
+++ login/index.js
@@ -0,0 +1,113 @@
+const loginPage = document.getElementById("loginPage");
+
+let forgetPassword = false;
+
+const box = document.createElement("div");
+box.id = "box";
+loginPage.append(box);
+
+const container = document.createElement("div");
+container.id = "container";
+box.append(container);
+
+const header = document.createElement("h1");
+header.id = "logo";
+header.innerText = "T&T Group";
+container.append(header);
+
+const bin = document.createElement("div");
+bin.id = "bin";
+container.append(bin);
+
+const email = document.createElement("div");
+email.classList.add("email");
+bin.append(email);
+
+const emailInput = document.createElement("input");
+emailInput.id = "emailInput";
+emailInput.placeholder = "Email*";
+email.append(emailInput);
+
+const password = document.createElement("div");
+password.classList.add("password");
+bin.append(password);
+
+const passwordInput = document.createElement("input");
+passwordInput.id = "passwordInput";
+passwordInput.setAttribute("type", "password");
+passwordInput.placeholder = "Password*";
+password.append(passwordInput);
+
+const forgotPassword = document.createElement("a");
+forgotPassword.id = "forgotPass";
+forgotPassword.innerText = "Forgot your password?";
+bin.append(forgotPassword);
+
+const loginButton = document.createElement("button");
+loginButton.id = "loginButton";
+loginButton.innerText = "Log In";
+container.append(loginButton);
+
+const dontHaveAcc = document.createElement("p");
+dontHaveAcc.id = "noAcc";
+dontHaveAcc.innerText = "Don't have an account? ";
+container.append(dontHaveAcc);
+
+const signUp = document.createElement("a");
+signUp.id = "signUp";
+signUp.href = "../sign-up/index.html";
+signUp.innerText = "Sign Up";
+dontHaveAcc.append(signUp);
+
+const image = document.createElement("img");
+image.id = "loginImage";
+image.src =
+  "https://images.unsplash.com/photo-1598804413160-07560f3f78ec?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1178";
+loginPage.append(image);
+
+const required = document.createElement("span");
+required.id = "required1";
+required.innerText = "Please enter your email.";
+email.append(required);
+
+const required2 = document.createElement("span");
+required2.id = "required2";
+required2.innerText = "Please enter your password.";
+password.append(required2);
+
+const email1 = document.getElementById("emailInput");
+const password1 = document.getElementById("passwordInput");
+const button1 = document.getElementById("loginButton");
+const requireEmail = document.getElementById("required1");
+const requirePassword = document.getElementById("required2");
+
+function validateFirstName(value) {
+  if (value === "") {
+    requireEmail.style.display = "block";
+  } else {
+    requireEmail.style.display = "none";
+  }
+}
+
+function validatePassword(value) {
+  if (value === "") {
+    requirePassword.style.display = "block";
+  } else {
+    requirePassword.style.display = "none";
+  }
+}
+button1.addEventListener("click", function () {
+  validateFirstName(email1.value);
+  validatePassword(password1.value);
+});
+
+forgotPassword.addEventListener("click", () => {
+  bin.style.display = "none";
+  loginButton.innerText = "Reset Password";
+  loginButton.style.textTransform = "none";
+  forgetPassword = true;
+  const enterEmail = document.createElement("input");
+  enterEmail.id = "emailInput";
+  enterEmail.placeholder = "Email";
+  header.append(enterEmail);
+});
---
+++ sign-up/index.js
@@ -0,0 +1,109 @@
+const signupPage = document.getElementById("signupPage");
+
+const box = document.createElement("div");
+box.id = "box";
+signupPage.append(box);
+
+const container = document.createElement("div");
+container.id = "container";
+box.append(container);
+
+const header = document.createElement("h1");
+header.id = "logo";
+header.innerText = "T&T Group";
+container.append(header);
+
+const contain = document.createElement("div");
+contain.id = "contain";
+container.append(contain);
+
+const firstLastName = document.createElement("div");
+firstLastName.classList.add("firstLastName");
+contain.append(firstLastName);
+
+const firstName = document.createElement("div");
+firstName.id = "firstName";
+firstLastName.append(firstName);
+
+const firstNameInput = document.createElement("input");
+firstNameInput.id = "firstNameInput";
+firstNameInput.placeholder = "First Name*";
+firstName.append(firstNameInput);
+
+const span = document.createElement("span");
+span.id = "span";
+firstName.append(span);
+
+const lastNameInput = document.createElement("input");
+lastNameInput.id = "lastNameInput";
+lastNameInput.placeholder = "Last Name*";
+firstName.append(lastNameInput);
+
+const email = document.createElement("div");
+email.id = "email";
+contain.append(email);
+
+const emailInput = document.createElement("input");
+emailInput.id = "emailInput";
+emailInput.placeholder = "Email*";
+email.append(emailInput);
+
+const password = document.createElement("div");
+password.id = "password";
+contain.append(password);
+
+const passwordInput = document.createElement("input");
+passwordInput.id = "passwordInput";
+passwordInput.placeholder = "Create a password*";
+passwordInput.setAttribute("type", "password");
+password.append(passwordInput);
+
+const required = document.createElement("span");
+required.id = "required1";
+required.innerText = "Please fill out all required fields.";
+container.append(required);
+
+const signUpButton = document.createElement("button");
+signUpButton.id = "signUpButton";
+signUpButton.innerText = "Sign Up";
+container.append(signUpButton);
+
+const haveAcc = document.createElement("p");
+haveAcc.id = "haveAcc";
+haveAcc.innerText = "Already have an account? ";
+container.append(haveAcc);
+
+const login = document.createElement("a");
+login.id = "logIn";
+login.href = "../login/index.html";
+login.innerText = "Log In";
+haveAcc.append(login);
+
+const image = document.createElement("img");
+image.id = "signupImage";
+image.src =
+  "https://images.unsplash.com/photo-1622915904739-efd69cd8c519?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960";
+signupPage.append(image);
+
+const firstNameInput1 = document.getElementById("firstNameInput");
+const lastNameInput1 = document.getElementById("lastNameInput");
+const email1 = document.getElementById("emailInput");
+const password1 = document.getElementById("passwordInput");
+const button1 = document.getElementById("signUpButton");
+const requireFirstName = document.getElementById("required1");
+
+function validateFirstName(value) {
+  if (value === "") {
+    requireFirstName.style.display = "flex";
+  } else {
+    requireFirstName.style.display = "none";
+  }
+}
+button1.addEventListener("click", function () {
+  validateFirstName(
+    firstNameInput1.value &&
+      lastNameInput1.value &&
+      email1.value &&
+      password1.value
+  );
+});
